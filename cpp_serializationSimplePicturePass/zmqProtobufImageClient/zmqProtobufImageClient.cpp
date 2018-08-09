1adc92d86c6aa7c6c8d1e322295de8da98e3e180
kuwingto <kuwingto@gmail.com>
2018-08-09 10:28:19 +0800

    add pyramid image to zmqProtobufImageClient fix bug in prop sheet add object info proto

--- cpp_serializationSimplePicturePass/zmqProtobufImageClient/zmqProtobufImageClient.cpp
+++ cpp_serializationSimplePicturePass/zmqProtobufImageClient/zmqProtobufImageClient.cpp
@@ -8,7 +8,16 @@
 #include <opencv2/opencv.hpp>
 #include "../zmqProtobufImageServer/objectInfos.pb.h"
 #include <chrono>  // for high_resolution_clock
-
+#include <math.h>
+struct ObjectInfo
+{
+	int objclass = 0;
+	float score = 0;
+	int tlx = 0;
+	int tly = 0;
+	int brx = 0;
+	int bry = 0;
+};
 int showImg(std::string windowName, cv::Mat img, int maxHeightShown, int maxWidthShown)
 {
 	if (img.empty())
@@ -37,6 +46,62 @@ int showImg(std::string windowName, cv::Mat img, int maxHeightShown, int maxWidt
 	cv::waitKey(1);
 }
 
+static int GetReductionFactorFromPyramidLevel(const int  pyramid_level)
+{
+	int ReductionFactor = 1;
+	switch (pyramid_level)
+	{
+	case 0:
+		ReductionFactor = 1;
+		break;
+	case 1:
+		ReductionFactor = 2;
+		break;
+	case 2:
+		ReductionFactor = 4;
+		break;
+	case 3:
+		ReductionFactor = 8;
+		break;
+	case 4:
+		ReductionFactor = 16;
+		break;
+	case 5:
+		ReductionFactor = 32;
+		break;
+	case 6:
+		ReductionFactor = 64;
+		break;
+	case 7:
+		ReductionFactor = 128;
+		break;
+	default:
+		ReductionFactor = 1;
+	}
+	return ReductionFactor;
+}
+
+void FormImgPyramid_With_Smoothing(
+	const cv::Mat &img,
+	int HighestLvL,
+	std::vector<cv::Mat> &img_query_pyramids)
+{
+	for (int i = 0; i <= HighestLvL; ++i)
+	{
+		cv::Mat img_query_py;
+		int reduction = GetReductionFactorFromPyramidLevel(i);
+		if (reduction != 1)
+		{
+			cv::pyrDown(img_query_pyramids.at(i - 1), img_query_py, cv::Size(img_query_pyramids.at(i - 1).cols / 2, img_query_pyramids.at(i - 1).rows / 2));
+		}
+		else
+		{
+			cv::blur(img, img_query_py, cv::Size(3, 3));
+		}
+
+		img_query_pyramids.push_back(img_query_py);
+	}
+}
 int main(void)
 {
 	zmq::context_t context(1);
@@ -44,12 +109,13 @@ int main(void)
 	/***********************
 	//  Socket to talk to server
 	**********************/
+	auto startInit = std::chrono::high_resolution_clock::now();
 	printf("Connecting to server \n");
 	zmq::socket_t sock(context, ZMQ_REQ);
 	sock.connect("tcp://localhost:5555");
 	
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
-	
+	auto finishInit = std::chrono::high_resolution_clock::now();
 	auto startWholeProcess = std::chrono::high_resolution_clock::now();
 	/***********************
 	//  Serialize
@@ -57,6 +123,7 @@ int main(void)
 	auto startEncode = std::chrono::high_resolution_clock::now();
 	std::vector<uchar> data_encode;
 	cv::Mat Imgdata;
+	int zoomOutLevel = 3;
 	{
 		std::string imagePath;
 		std::cout << "1|2|3|4|5" << "\n";
@@ -65,7 +132,10 @@ int main(void)
 		imagePath = std::string("0") + std::to_string(imageId) + ".bmp";
 		Imgdata = cv::imread(imagePath, CV_LOAD_IMAGE_COLOR);
 
-		cv::imencode(".bmp", Imgdata, data_encode);
+		std::vector<cv::Mat> imagePyList;
+		FormImgPyramid_With_Smoothing(Imgdata, zoomOutLevel, imagePyList);
+		cv::imencode(".bmp", imagePyList.at(zoomOutLevel), data_encode);
+		//cv::imencode(".bmp", Imgdata, data_encode);
 	}
 	auto finishEncode = std::chrono::high_resolution_clock::now();
 	
@@ -88,29 +158,44 @@ int main(void)
 	std::string  msgStr = std::string((char*)reply.data(), reply.size());
 	auto endWholeProcess = std::chrono::high_resolution_clock::now();
 	//unserialize
-	proto::objectInfos stObjectInfos;
-	stObjectInfos.ParseFromString(msgStr);
+	proto::objectInfos MsgObjectInfos;
+	MsgObjectInfos.ParseFromString(msgStr);
+	//scale up
+	std::vector<ObjectInfo> vObjectInfos;
+	for (int i = 0; i < MsgObjectInfos.infos_size(); ++i)
 	{
-		for (int i = 0; i < stObjectInfos.infos_size(); ++i)
+		ObjectInfo info;
+		info.objclass = MsgObjectInfos.infos(i).objclass();
+		info.score = MsgObjectInfos.infos(i).score();
+		info.tlx = MsgObjectInfos.infos(i).tlx()  * std::pow(2, zoomOutLevel);
+		info.tly = MsgObjectInfos.infos(i).tly() * std::pow(2, zoomOutLevel);
+		info.brx = MsgObjectInfos.infos(i).brx() * std::pow(2, zoomOutLevel);
+		info.bry = MsgObjectInfos.infos(i).bry() * std::pow(2, zoomOutLevel);
+		vObjectInfos.push_back(info);
+	}
+	
+	{
+		for (int i = 0; i < vObjectInfos.size(); ++i)
 		{
-			std::cout << stObjectInfos.infos(i).objclass() << " "
-				<< stObjectInfos.infos(i).score() << " "
-				<< stObjectInfos.infos(i).tlx() << " "
-				<< stObjectInfos.infos(i).tly() << " "
-				<< stObjectInfos.infos(i).brx() << " "
-				<< stObjectInfos.infos(i).bry() << " "
+			std::cout << vObjectInfos.at(i).objclass << " "
+				<< vObjectInfos.at(i).score << " "
+				<< vObjectInfos.at(i).tlx << " "
+				<< vObjectInfos.at(i).tly << " "
+				<< vObjectInfos.at(i).brx << " "
+				<< vObjectInfos.at(i).bry << " "
 				<< "\n";
 		}
+
 		cv::Mat shownImage;
 		Imgdata.copyTo(shownImage);
 
-		for (int i = 0; i < stObjectInfos.infos_size(); ++i)
+		for (int i = 0; i < vObjectInfos.size(); ++i)
 		{
 			cv::Rect r = cv::Rect(
-				stObjectInfos.infos(i).tlx(),
-				stObjectInfos.infos(i).tly(),
-				stObjectInfos.infos(i).brx() - stObjectInfos.infos(i).tlx(),
-				stObjectInfos.infos(i).bry() - stObjectInfos.infos(i).tly());
+				vObjectInfos.at(i).tlx,
+				vObjectInfos.at(i).tly,
+				vObjectInfos.at(i).brx - vObjectInfos.at(i).tlx,
+				vObjectInfos.at(i).bry - vObjectInfos.at(i).tly);
 			cv::rectangle(shownImage, r, cv::Scalar(255, 0, 0), 5, 8, 0);
 		}
 
@@ -122,7 +207,8 @@ int main(void)
 	std::cout << "send time elapsed = " << elapsedSend.count() << "\n";
 	std::chrono::duration<double> elapsedWholeProcess = endWholeProcess - startWholeProcess;
 	std::cout << "whole process time elapsed = " << elapsedWholeProcess.count() << "\n";
-	
+	std::chrono::duration<double> elapsedInit = finishInit - startInit;
+	std::cout << "Init time elapsed = " << elapsedInit.count() << "\n";
 	sock.close();
 	system("pause");
 	return 0;
